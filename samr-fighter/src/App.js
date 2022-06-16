21967d80714735d8309365f32259e593f48bf310
Alex Burroughs <alex@deepspacerobots.com>
2022-06-16 13:00:19 -0500

    Added samr animations

--- samr-fighter/src/App.js
+++ samr-fighter/src/App.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "./app.scss";
 import LevelSelect from "./pages/levelSelect";
+import logo from "./assets/logo.svg";
 
 function App() {
+  const [showLogo, setShowLogo] = useState(true);
+  const [fadeOutLogo, setFadeOutLogo] = useState(false);
+  const [level, setLevel] = useState(null);
+
+  const onLogoClick = () => {
+    setFadeOutLogo(true);
+    setTimeout(() => setShowLogo(false), 500);
+  };
+
   return (
     <div className="layout">
-      <LevelSelect />
+      {!level ? <LevelSelect setLevel={setLevel} /> : <LevelSelect />}
+      {showLogo && (
+        <div
+          className={`game-logo-container ${fadeOutLogo && "fade-out"}`}
+          onClick={onLogoClick}
+        >
+          <img src={logo} alt="samr-fighter" className="game-logo" />
+          <p>(Click to begin)</p>
+        </div>
+      )}
     </div>
   );
 }
--- samr-fighter/src/pages/levelSelect/levelSelect.jsx
+++ samr-fighter/src/pages/levelSelect/levelSelect.jsx
@@ -1,15 +1,48 @@
 import levelConfig from "../../data/levelConfig";
 import samr from "../../assets/samr.svg";
 import salesforce from "../../assets/salesforce.svg";
+import { useEffect, useState } from "react";
 
-const LevelSelect = () => {
+const actionWords = [
+  "Boom",
+  "Pow",
+  "Blam",
+  "Whack",
+  "Slam",
+  "bif",
+  "bam",
+  "whomp",
+  "thump",
+  "smash",
+  "zowie",
+  "bang",
+  "wham",
+];
+
+const LevelSelect = ({ setLevel }) => {
+  const [actionWord, setActionWord] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setActionWord(Math.floor(Math.random() * actionWords.length));
+    }, 2000);
+  }, []);
+
   return (
     <div className="level-select">
       <h1 className="choose-level-text">Choose your level:</h1>
+      <img className="samr-animated" src={samr} alt="samr" />
+      <img className="samr-animated salesforce" src={salesforce} alt="samr" />
+      <p className="fight-text">{actionWords[actionWord]}!</p>
+
       <div className="level-select-card-container">
         {levelConfig.map(
           ({ title, image, movesPerTurn, salesforceHealth, difficulty }, i) => (
-            <div className="level-select-card" key={i}>
+            <div
+              className="level-select-card"
+              key={i}
+              onClick={() => setLevel(levelConfig[i])}
+            >
               <img className="level-select-background" src={image} alt="" />
 
               <div className="level-select-card-background">
