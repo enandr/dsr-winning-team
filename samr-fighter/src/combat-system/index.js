119bc50958f9a9e74c19b18a45515a206efce150
rogerenand <rogerenand@gmail.com>
2022-06-16 12:47:43 -0500

    added readme

--- samr-fighter/src/combat-system/index.js
+++ samr-fighter/src/combat-system/index.js
@@ -1,4 +1,18 @@
 import React, {useEffect, useState} from 'react';
+/*
+* TO USE COMBAT SYSTEM;
+* const [combat] = useState(new CombatSystem('easy'|'med'|'hard'|'very hard'|'extreme))
+* to get player health:
+*   combat.getSamrHealth() || combat.getEnemyHealth()
+* to get player availabe moves:
+*   combat.availableMoves('samr') || combat.availableMoves('enemy')
+* to get moves per turn:
+*   combat.getMovesPerTurn()
+* to attack:
+*   Possible Moves: punch | kick | block | duck
+*   the arguments MUST BE A STRING ARRAY of moves. It CANNOT be of greater length than moves per turn
+*   combat.attack(ArrayOfMovesForSamr,ArrayOfMovesForEnemy)
+* */
 class CombatSystem {
     #moves = ['punch','kick','block','duck'];
     #samrHealth = 10;
