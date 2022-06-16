d9e632f4afed0a829036b824a2431f5ddca74e0a
rogerenand <rogerenand@gmail.com>
2022-06-16 12:47:57 -0500

    added demo tests

--- samr-fighter/src/App.js
+++ samr-fighter/src/App.js
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+import CombatSystem from "./combat-system";
 
 function App() {
+  const [combat] = useState(new CombatSystem('easy'))
+  useEffect(() => {
+    console.log('samr available moves',combat.availableMoves('samr'))
+    console.log('enemy available moves',combat.availableMoves('enemy'))
+    console.log('enemy health',combat.getEnemyHealth())
+    console.log('samr health',combat.getSamrHealth())
+    console.log('moves per turn',combat.getMovesPerTurn())
+    combat.attack(['punch'],['kick'])
+    console.log('samr available moves',combat.availableMoves('samr'))
+    console.log('enemy available moves',combat.availableMoves('enemy'))
+    console.log('enemy health',combat.getEnemyHealth())
+    console.log('samr health',combat.getSamrHealth())
+    console.log('moves per turn',combat.getMovesPerTurn())
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
