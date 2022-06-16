8cc2b3133ccf2dc4b3ba57b49a8d4a5ad5b68c97
rogerenand <rogerenand@gmail.com>
2022-06-16 12:51:08 -0500

    fixed merge conflict

--- samr-fighter/src/App.js
+++ samr-fighter/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.scss";
+import LevelSelect from "./pages/levelSelect";
 import React, {useEffect, useState} from "react";
 import CombatSystem from "./combat-system";
 
@@ -20,21 +20,8 @@ function App() {
   },[])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <LevelSelect />
     </div>
   );
 }
